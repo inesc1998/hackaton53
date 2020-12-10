e4e9bc201e65c02505164baa5454a32754f7ef8f
João Armas Sousa <joao.gustavo.armas.silva.sousa@gmail.com>
2020-12-10 17:18:31 -0100

    Added GodFather and GodfatherService classes

---
+++ voiceToTheSilent/src/main/java/org/academiadecodigo/bitjs/voiceToTheSilent/service/GodfatherService.java
@@ -0,0 +1,22 @@
+package org.academiadecodigo.bitjs.voiceToTheSilent.service;
+
+import org.academiadecodigo.bitjs.voiceToTheSilent.model.Godfather;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class GodfatherService {
+     private List<Godfather> godfathersList = new LinkedList<>();
+
+     public void populateList(Godfather newGodfather){
+         this.godfathersList.add(newGodfather);
+     }
+
+    public List<Godfather> getGodfathersList() {
+        return godfathersList;
+    }
+
+    public void setGodfathersList(List<Godfather> godfathersList) {
+        this.godfathersList = godfathersList;
+    }
+}
