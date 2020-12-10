809eaf0de74e872fcb798f7f01744a5d15826fd4
Maria Inês Costa <inesc1998@gmail.com>
2020-12-10 12:43:34 -0100

    Added the casesService

---
+++ voiceToTheSilent/src/main/java/org/academiadecodigo/bitjs/voiceToTheSilent/service/casesService.java
@@ -0,0 +1,8 @@
+package org.academiadecodigo.bitjs.voiceToTheSilent.service;
+
+import java.util.HashMap;
+
+public class casesService {
+
+    private HashMap<Integer, IsolatedCase> cases = new HashMap<>();
+}
